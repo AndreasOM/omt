5e9d9a15665752bdf2fc30d2ebd95c525cf16bf7
Andreas Neukoetter <andreas@omni-mad.com>
2020-04-02 17:04:43 +0200

    : Ignored Data for git. + Added first version of shader tool.

--- src/lib.rs
+++ src/lib.rs
@@ -3,5 +3,6 @@ pub mod atlas;
 pub mod font;
 pub mod gfx;
 pub mod packer;
+pub mod shader;
 pub mod soundbank;
 pub mod util;
---
+++ src/shader/shader.rs
@@ -0,0 +1,40 @@
+use crate::util::OmError;
+
+use shader_crusher::ShaderCrusher;
+
+use std::fs;
+
+pub struct Shader {
+
+}
+
+impl Shader {
+
+	pub fn build(
+		input: &str,
+		mode: &str,
+		output: &str
+	) -> Result<u32, OmError>{
+
+		let mut sc = ShaderCrusher::new();
+		let data = match fs::read_to_string(input) {
+			Ok( data ) => data,
+			Err( e ) => return Err(OmError::Generic(e.to_string())),
+		};
+
+		sc.set_input( &data );
+		// :TODO: use correct mode
+		sc.crush();
+		// :TODO: check return value
+		if sc.get_output().len() > 0 {
+			// :TODO: use correct mode
+			fs::write(output, data).expect("// Unable to write file");
+//			fs::write(output, sc.get_output()).expect("// Unable to write file");
+			return Ok( 1 );
+		} else {
+			return Err(OmError::Generic("Error in shader".to_string()));
+		}
+
+	}
+}
+
